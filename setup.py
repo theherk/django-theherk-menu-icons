68d97240ffdfab880196127bff4b67f7b88837a7
Adam Sherwood <theherk@gmail.com>
2013-05-21 17:18:53 -0500

    Updated to include all packages and templates.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 def read(fname):
@@ -7,8 +7,8 @@ def read(fname):
 
 setup(
     name='django-theherk-menu-icons',
-    version='1.1',
-    packages=['menu_icons'],
+    version='1.2',
+    packages=find_packages(),
     include_package_data=True,
     license='see file LICENSE',
     description='Django CMS navigation modifier to attach icons to menus.',
