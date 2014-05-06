6a098aa9265a146d886db99d0646672e58f3e46e
Adam Sherwood <theherk@gmail.com>
2014-05-05 23:30:46 -0500

    Update version number for distutils to 1.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name='django-theherk-menu-icons',
-    version='1.2',
+    version='1.3',
     packages=find_packages(),
     include_package_data=True,
     license='see file LICENSE',
